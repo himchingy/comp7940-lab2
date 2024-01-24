f89ff40e0bbb41323890c7284c141c9b6fd8a99c
himchingy <136294234+himchingy@users.noreply.github.com>
2024-01-24 09:43:34 +0800

    Python Exercise

---
+++ exer1.py
@@ -0,0 +1,42 @@
+import json
+import requests
+site="https://api.npoint.io/2b57052af2060e84dc86"
+# Write the functions convert_number and replace_number here
+def convert_number(inputString):
+    outputNumber = []
+    for i in range(len(inputString) - 1):
+        outputNumber.append(int(inputString[i+1]))
+    return outputNumber
+
+def replace_number(number_list, being_replace, to_replace):
+    new_number_list = []
+    for num in number_list:
+        if (num == being_replace):
+            new_number_list.append(to_replace)
+        else:
+            new_number_list.append(num)
+    return new_number_list
+
+# Follow the logic below.
+# Trying to load JSON into text
+r = requests.get(site)
+print(r.json())
+text = r.json()['users']
+# Debug
+for i in text:
+    print("parse " + str(i))
+# call the function convert_number
+# convert all elements (except the first one) into number and return it as a list
+y = convert_number(text[0])
+print("y")
+print(y)
+# call the function replace_number
+# replace all number 1 by the number 10 in the function
+z = replace_number(number_list = y, being_replace = 1, to_replace = 10)
+print("z")
+print(z)
+sum = 0
+for i in z:
+    sum = sum + i
+    print("sum = " + str(sum) + "; i =" + str(i))
+print ("Total = " + str(sum))
